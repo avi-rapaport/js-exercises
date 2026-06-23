42bdf2c2e89bcceca7a78546ea2833ad8a96c7c5
avi rapaport <kenblock.ar@gmail.com>
2026-06-23 11:57:19 +0300

    added exercise 2

--- array_methods.js
+++ array_methods.js
@@ -2,3 +2,8 @@
 const arr = [1, 2, 3, 4, 5];
 const double = arr.map((num) => num * 2);
 console.log(double);
+
+//2
+const arr2 = ["hello", "world"];
+arr2.forEach((str, i) => (arr2[i] = str.toUpperCase()));
+console.log(arr2);
