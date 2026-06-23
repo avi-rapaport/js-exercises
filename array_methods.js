da4a17fb2312717cd08c89edea6af1ff1ecd9870
avi rapaport <kenblock.ar@gmail.com>
2026-06-23 12:07:49 +0300

    added exercise 8

--- array_methods.js
+++ array_methods.js
@@ -12,3 +12,10 @@ console.log(arr2);
 const arr3 = [1, 2, 3, 4, 5, 6];
 const fArr3 = arr3.filter((num) => num % 2 == 0);
 console.log(fArr3);
+
+//8
+const prices = [10, 50, 105, 80];
+const fmPrices = prices
+  .filter((price) => price > 100)
+  .map((price) => price + price * 0.1);
+console.log(fmPrices);
