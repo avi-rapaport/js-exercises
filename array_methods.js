9a717128cd5049b6a9f7e7f540229b9e437297d7
avi rapaport <kenblock.ar@gmail.com>
2026-06-23 12:02:02 +0300

    added exercise 4

--- array_methods.js
+++ array_methods.js
@@ -7,3 +7,8 @@ console.log(double);
 const arr2 = ["hello", "world"];
 arr2.forEach((str, i) => (arr2[i] = str.toUpperCase()));
 console.log(arr2);
+
+//4
+const arr3 = [1, 2, 3, 4, 5, 6];
+const fArr3 = arr3.filter((num) => num % 2 == 0);
+console.log(fArr3);
